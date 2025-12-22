9dcface96c8fb04100c1493058a3ff6b543152cc
Shaikh Naasir <yoursdeveloper@protonmail.com>
2025-12-22 17:59:31 +0530

    Remove base path configuration from astro.config
    Signed-off-by: Shaikh Naasir <yoursdeveloper@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://shaikhnaasir.github.io',
-  base: '/',
   output: 'static',
   trailingSlash: 'always',
 });
